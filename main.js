e898b022cfc3acc7dd38ad0af12f915533ba858d
mabeltsado2 <mtsado@andrew.cmu.edu>
2021-11-06 18:04:33 -0400

    adding adding local storage to JS

--- main.js
+++ main.js
@@ -1,72 +1,76 @@
-function cinnaRoll(quantity, glaze, price, imgsrc){
+function cinnaRoll(quantity, glaze){
     this.quantity = quantity;
     this.glaze = glaze;
-    this.price = price;
-    this.imgsrc = imgsrc;
 }
-
 
 const cartArr = [];
 
-function updateCartTab() {
+//function updateCartTab() {
 
-    let cartNum = cartArr.length; 
+    //cartArr = JSON.parse(localStorage.getItem("cartArr") || "[]");
+    //let cartNum = cartArr.length; 
 
-    if (cartNum > 0){
-        document.getElementById('cart-label').href = "cart.html";
-        document.getElementById('cart-label').textContent = "Cart("+cartNum+ ")";
+    //if (cartNum > 0){
+       // document.getElementById('cart-label').href = "cart.html";
+        //document.getElementById('cart-label').textContent = "Cart("+cartNum+ ")";
         
-    } else {
-        document.getElementById('cart-label').innerHTML = "Cart"
-    }
-}
+    //} else {
+       // document.getElementById('cart-label').textContent = "Cart"
+    //}
+//}
 
 function myAlert() {
     alert("Items Added to Cart!");
   }
 
+var quantityValue
+function quantitySelected() {
+    quantityValue = Number(document.getElementById("dropdown-quantity").value)
+    document.getElementById("price").innerHTML = "$" + calculatePrice(quantityValue)
+}
 
-var quantityValue = document.getElementById("dropdown-quantity").value
+var glazeValue
+function imgChange () {
+    glazeValue = document.getElementById("glaze-name").value
+    glazeValue = glazeValue.split(" ")[0]
+    document.getElementById("pumpkin").src = glazeValue + ".jpg"
+}
 
-var glazeValue = document.getElementById("glaze").value
 
 // create json object based on selected values
 
-switch(quantityValue){
-    case "1": 
-        var priceValue = 5.99 * 1 ;
-        break;
-    case "3": 
-        var priceValue = 5.99 * 3 ;
-        break;
-    case "6": 
-        var priceValue = 5.99 * 6 ;
-        break;
-    case "12": 
-        var priceValue = 5.99 * 12 ;
-        break;
-
+function calculatePrice (quantityValue) {
+    return quantityValue * 5.99
 }
 
-switch(glazeValue){
-    case "none": 
-        var imgsrcValue = "pumpkin-spice-roll.jpg" ;
-        break;
-    case "vanilla-milk": 
-        var imgsrcValue = "pumpkin1.jpg" ;
-        break;
-    case "sugar-milk": 
-        var imgsrcValue = "pumpkin2.jpg" ;
-        break;
-    case "double-chocolate": 
-        var imgsrcValue = "pumpkin3.jpg" ;
-        break;
-
+function addItem() {
+    // get stored value of cart items
+    var cartItemsString = localStorage.getItem("cartItems")
+    // check if item exists in storage, if so, parse it
+    if (cartItemsString === null) { // first time, we have to create the cartItems list
+      var cartItems = []
+    } else { // we've been here before, load the value
+      var cartItems = JSON.parse(cartItemsString)
+    }
 }
+// switch glazeValue {
+//     case "none": 
+//         var imgsrcValue = "pumpkin-spice-roll.jpg" ;
+//         break;
+//     case "vanilla-milk": 
+//         var imgsrcValue = "pumpkin1.jpg" ;
+//         break;
+//     case "sugar-milk": 
+//         var imgsrcValue = "pumpkin2.jpg" ;
+//         break;
+//     case "double-chocolate": 
+//         var imgsrcValue = "pumpkin3.jpg" ;
+//         break;
 
-let itemSelected = new cinnaRoll (quantityValue, glazeValue, priceValue, imgsrcValue)
+// }
+
+let itemSelected = new cinnaRoll (quantityValue, glazeValue)
 // add this newly selected item to the cart
 cartArr.push(itemSelected)
 
 updateCartTab ();
- 
