ecb86a7b051d07dd6879af78050780a5b673e76c
mabeltsado2 <mtsado@andrew.cmu.edu>
2021-11-06 14:42:11 -0400

    adding selection to JS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function cinnaRoll(quantity, options, price, imgsrc){
+function cinnaRoll(quantity, glaze, price, imgsrc){
     this.quantity = quantity;
-    this.options = options;
+    this.glaze = glaze;
     this.price = price;
     this.imgsrc = imgsrc;
 }
@@ -9,24 +9,64 @@ function cinnaRoll(quantity, options, price, imgsrc){
 const cartArr = [];
 
 function updateCartTab() {
- 
-    cartArr.push(1)
 
-    let cartNum = cartArr.length - 1; 
+    let cartNum = cartArr.length; 
 
     if (cartNum > 0){
         document.getElementById('cart-label').href = "cart.html";
         document.getElementById('cart-label').textContent = "Cart("+cartNum+ ")";
         
     } else {
-        document.getElementById('cart-label').innerHTML
-        = "Cart"
+        document.getElementById('cart-label').innerHTML = "Cart"
     }
 }
 
-updateCartTab ();
- 
 function myAlert() {
     alert("Items Added to Cart!");
   }
 
+
+var quantityValue = document.getElementById("dropdown-quantity").value
+
+var glazeValue = document.getElementById("glaze").value
+
+// create json object based on selected values
+
+switch(quantityValue){
+    case "1": 
+        var priceValue = 5.99 * 1 ;
+        break;
+    case "3": 
+        var priceValue = 5.99 * 3 ;
+        break;
+    case "6": 
+        var priceValue = 5.99 * 6 ;
+        break;
+    case "12": 
+        var priceValue = 5.99 * 12 ;
+        break;
+
+}
+
+switch(glazeValue){
+    case "none": 
+        var imgsrcValue = "pumpkin-spice-roll.jpg" ;
+        break;
+    case "vanilla-milk": 
+        var imgsrcValue = "pumpkin1.jpg" ;
+        break;
+    case "sugar-milk": 
+        var imgsrcValue = "pumpkin2.jpg" ;
+        break;
+    case "double-chocolate": 
+        var imgsrcValue = "pumpkin3.jpg" ;
+        break;
+
+}
+
+let itemSelected = new cinnaRoll (quantityValue, glazeValue, priceValue, imgsrcValue)
+// add this newly selected item to the cart
+cartArr.push(itemSelected)
+
+updateCartTab ();
+ 
