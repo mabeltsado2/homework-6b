97e8427d9b6816d38a732b159ae2aa8b4f61d4a3
mabeltsado2 <mtsado@andrew.cmu.edu>
2021-11-06 20:34:55 -0400

    update cart

--- cart.js
+++ cart.js
@@ -1,23 +1,13 @@
 
-// Getting stuff from local storage
+// Getting item from local storage
 const storedValue1 = JSON.parse(localStorage.getItem("cart"));
 const cartArray1 = storedValue1 ? storedValue1 : []; 
 
 const cartDiv = document.getElementById('cart-container');
 
 function showProductInCart(product) {
-    let thecode = '<img src = "pumpkin-spice-roll.jpg" alt= "pumpkin spice Cinnamon roll" class = "roll-pics" id="cart-image"><div id = "cart-text" class = "roll-pics"><b>Pumpkin Spice Cinnamon Roll</b><br><div>Glaze'+ product.glaze+'</div></div><div id = "item-price">$5.99</div><div id="item-quantity">'+ product.quantity+'</div><div id = "item-total">$'+ product.price+'</div><br>';
+    let thecode = '<img src = "pumpkin-spice-roll.jpg" alt= "pumpkin spice Cinnamon roll" class = "roll-pics" id="cart-image"><div id = "cart-text" class = "roll-pics"><b>Pumpkin Spice Cinnamon Roll</b><br><div>Glaze: '+ product.glaze+'</div><div id = "remove" >Remove</div></div><div id = "item-price">$5.99</div><div id="item-quantity">'+ product.quantity+'</div><div id = "item-total">$'+ product.price+'</div><br>';
     document.getElementById("cart-container").innerHTML += thecode; 
-    
-    // const clone = template.content.cloneNode(true);
-    // clone.querySelector('.item_name').innerText = product.name;
-    // clone.querySelector('.item_details').innerText = "Color: " + product.color + " | Size: " + product.size;
-    // clone.querySelector('.item_price').innerText = product.price;
-    // const button = clone.querySelector('.item_remove');
-    //button.addEventListener('click', function() {
-    // find and remove object from cart
-    //});
-    //cartDiv.appendChild(clone);
 }
 
     for (let i = 0; i < cartArray1.length; i++) {
